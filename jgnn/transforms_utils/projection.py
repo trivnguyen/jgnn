d2e85d964182ae9835e62432cd5d44b33bb2fd23
Tri Nguyen <tnguyen@workergpu038.cm.cluster>
2025-10-13 14:55:44 -0400

    add option to project along specific axis

--- jgnn/transforms_utils/projection.py
+++ jgnn/transforms_utils/projection.py
@@ -17,22 +17,27 @@ def random_rotation_matrix():
 
 class RandomProjection:
     """ Apply a random projection to the input batch """
-    def __init__(self):
-        pass
+    def __init__(self, axis=None):
+        self.axis = axis
 
     def __call__(self, batch):
         batch = batch.clone()
 
-        # create the random projection matrix
-        R = random_rotation_matrix()
+        if self.axis is None:
 
-        # apply rotation to position and velocity
-        pos_proj = torch.matmul(batch.pos, R)
-        vel_proj = torch.matmul(batch.vel, R)
+            # create the random projection matrix
+            R = random_rotation_matrix()
 
-        # apply the projection by removing the last dimension
-        pos_proj = pos_proj[:, :2]
-        vel_proj = vel_proj[:, 2].unsqueeze(1)
+            # apply rotation to position and velocity
+            pos_proj = torch.matmul(batch.pos, R)
+            vel_proj = torch.matmul(batch.vel, R)
+
+            # apply the projection by removing the last dimension
+            pos_proj = pos_proj[:, :2]
+            vel_proj = vel_proj[:, 2].unsqueeze(1)
+        else:
+            pos_proj = torch.cat([batch.pos[:, :self.axis], batch.pos[:, self.axis+1:]], dim=1)
+            vel_proj = batch.vel[:, self.axis].unsqueeze(1)
 
         # update the batch
         batch.pos = pos_proj
