ba874072c414a0326d86ee625b5fa466421e1d11
Tri Nguyen <tnguyen@workergpu032.cm.cluster>
2025-10-24 16:35:47 -0400

    slightly change order of node attr for consistency

--- datasets/preprocess.py
+++ datasets/preprocess.py
@@ -38,6 +38,6 @@ def create_graph_from_posvel(
         x = torch.cat([log_rad, vel, vel_error], dim=1)
     else:
         x = torch.cat([log_rad,  vel], dim=1)
-    graph = Data(x=x, theta=label, pos=pos, vel=vel, cond=cond)
+    graph = Data(x=x, pos=pos, vel=vel, theta=label, cond=cond)
 
     return graph
