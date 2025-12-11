4cc98f85c3846535db7c9ffe0fe8b412f05e6944
Tri Nguyen <tnguyen@ccalin011.flatironinstitute.org>
2025-12-10 19:39:53 -0500

    add experimental NRE class

--- jgnn/models/lightning/__init__.py
+++ jgnn/models/lightning/__init__.py
@@ -3,12 +3,14 @@
 from .gnn_embedding import GNNEmbedding
 from .transformer_embedding import TransformerEmbedding
 from .npe import NPE
+from .nre import NRE
 from .snpe import SequentialNPE, DirectPosterior
 
 __all__ = [
     'GNNEmbedding',
     'TransformerEmbedding',
     'NPE',
+    'NRE',
     'SequentialNPE',
     'DirectPosterior',
 ]
